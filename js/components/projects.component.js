74c71ee8c657eef46095f109e1d554a931d1b637
lanefaison2 <faisonusmc@gmail.com>
2017-04-27 00:20:22 -0600

    css updates, responsive updates

--- js/components/projects.component.js
+++ js/components/projects.component.js
@@ -15,9 +15,9 @@
 
       {title: 'gReads A Lot', description: 'This application allows users to interact with and search a database full of authors and books, as well as create their own authors and books.', image_url:'/assets/images/logos-projects/greads.png', project_url:'https://g-reads-alot.herokuapp.com/index.html'},
 
-      {title: 'gDrinks', description: 'This goal of this project was to create an interactive site that allows the user to add, edit, and delete recipes.', image_url:'http://cdn.liquor.com/wp-content/uploads/2012/09/bourbon-old-fashioned.jpg', project_url:'https://gdrinks-d4c1b.firebaseapp.com/index.html'},
+      {title: 'gDrinks', description: 'This goal of this project was to create an interactive site that allows the user to add, edit, and delete recipes.', image_url:'/assets/images/logos-projects/drink2.png', project_url:'https://gdrinks-d4c1b.firebaseapp.com/index.html'},
 
-      {title: 'Movie Ratings', description: 'The goal of this project was to create a site where the user can create, read, update, and delete movies from the database.', image_url:'/assets/images/logos-projects/americanSniper.jpg', project_url:'https://salty-chamber-44152.herokuapp.com/home.html'},
+      {title: 'Movie Ratings', description: 'The goal of this project was to create a site where the user can create, read, update, and delete movies from the database.', image_url:'/assets/images/logos-projects/cinema.png', project_url:'https://salty-chamber-44152.herokuapp.com/home.html'},
 
       {title: 'galvanize Eats', description: 'This site was built as a 1-day project. My goal was to build a site for "Galvanize Eats" that used APIs to get a menu and post a customer\'s order.', image_url:'/assets/images/logos-projects/galvanizeEats.png', project_url:'https://galvanizeeats-4818e.firebaseapp.com/'},
 
