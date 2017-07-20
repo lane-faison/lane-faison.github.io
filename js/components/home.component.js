c3c26e2ff35a38fb3fa29ed5ccbc72317023cc44
Lane Faison <lane.faison@icloud.com>
2017-07-19 19:58:05 -0600

    Small UI updates, position updates, skills update

--- js/components/home.component.js
+++ js/components/home.component.js
@@ -16,10 +16,11 @@
 
     vm.skills_Management = ["GitHub","git","Firebase","Heroku","Atom","Balsamiq","Excel"]
 
-    vm.skills_Mobile = ["Swift 3","Xcode","Core Data","Core Location","Apple Maps SDK","Facebook SDK","Google Maps API","Google Places API","Google Cloud Vision API","CocoaPods","Alamofire"]
+    vm.skills_Mobile = ["Swift 3","Xcode","Core Data","Core Location","Apple Maps SDK","Facebook SDK","Google Maps API","Google Places API","Google Cloud Vision API","CocoaPods","Alamofire","AutoLayout"]
 
     vm.jobs = [
-      {title: "Full Stack Web Developer student", company: "Galvanize, Inc.", date: "January 2017 - Present"},
+      {title: "iOS Engineer", company: "Gloo", date: "July 2017 - Present"},
+      {title: "Full Stack Web Developer student", company: "Galvanize, Inc.", date: "January 2017 - June 2017"},
       {title: "Sr. Technical Recruiter", company: "Talent Navigation Experts", date: "June 2015 - January 2017"},
       {title: "Ski Instructor", company: "Vail Resorts", date: "December 2013 - March 2015"},
       {title: "Sergeant", company: "United States Marine Corps", date: "March 2008 - January 2012"}
